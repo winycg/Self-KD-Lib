5ed4b3dfa186a30315ba78e26433fa351a7bd4b9
winycg <winycg@163.com>
2022-10-21 21:17:55 +0800

    update

--- scripts/main_mixskd_imagenet.sh
+++ scripts/main_mixskd_imagenet.sh
@@ -3,6 +3,12 @@ python main_mixskd_imagenet.py \
     --arch MixSKD_ImageNet_ResNet50 \
     --dist-url 'tcp://127.0.0.1:2222' \
     --dist-backend 'nccl' \
+    --batch-size 128 \
+    --init-lr 0.1 \
+    --warmup-epoch 5 \
+    --lr-type cosine \
+    --epochs 305 \
+    --mixup-alpha 0.2 \
     --checkpoint-dir [your checkpoint saving path] \
     --multiprocessing-distributed \
     --gpu-id 0,1,2,3,4,5,6,7 \
